9f68f0bcbe7496a7233abfc20a80d4ceaf2c76de
marshot <124557537+marshot9420@users.noreply.github.com>
2024-02-19 14:09:32 +0900

    refactor: 불필요한 스타일 변수 제거

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -2,28 +2,13 @@ import { css } from '@emotion/react';
 
 import { styles } from '@/styles';
 
-import { responsive } from './variables';
-
-export const grid = css`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(min(100%, 10rem), 1fr));
-
-  ${responsive.TABLET} {
-    grid-template-columns: repeat(6, minmax(0, 1fr));
-  }
-
-  ${responsive.MOBILE} {
-    grid-template-columns: repeat(2, minmax(0, 1fr));
-  }
-`;
-
 export const innerWrapper = (colGap = styles.grid.gutter) => css`
   width: ${styles.maxWidth}rem;
   padding: 0 ${colGap / 2}rem;
   margin: 0 auto;
 `;
 
-export const gridSection = (
+export const grid = (
   cols = styles.grid.column,
   rowGap = styles.grid.rowGap,
   colGap = styles.grid.gutter,
