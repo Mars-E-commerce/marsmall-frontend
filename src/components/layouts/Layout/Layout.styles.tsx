813fea68faf6f561f32429adad470932572fd2ba
marshot <124557537+marshot9420@users.noreply.github.com>
2024-02-21 21:34:59 +0900

    feat: Layout 공통 컴포넌트 추가

---
+++ src/components/layouts/Layout/Layout.styles.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const LayoutContainer = styled.div`
+  min-height: inherit;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Main = styled.main`
+  flex-grow: 1;
+
+  display: flex;
+  flex-direction: column;
+`;
--- src/routes/routers.tsx
+++ src/routes/routers.tsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import { Layout } from '@/components';
+
 import { Home } from '@/pages';
+import { URLS } from '@/constants';
 
 export const routers = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <Layout />,
+    children: [{ path: URLS.CLIENT.HOME, element: <Home /> }],
   },
 ]);
