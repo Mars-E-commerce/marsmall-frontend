829841b8c9bbff72ffdef6a6e032126f181eee28
marshot <124557537+marshot9420@users.noreply.github.com>
2024-02-21 21:34:35 +0900

    refactor: Footer 컴포넌트 내용 상수화

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import { memo } from 'react';
 
+import { APP } from '@/constants';
+
 import { FooterContainer, FooterContent, FooterInner } from './Footer.styles';
 
 const Footer = () => {
@@ -7,21 +9,17 @@ const Footer = () => {
     <FooterContainer>
       <FooterInner>
         <FooterContent>
-          <div>© 2024 MARSHOT. All rights reserved.</div>
-          <div>MarsMall</div>
-          <div>주소: 경남 김해시 김해대로 1234번길 12-3</div>
-          <div>사업자 등록 번호: 123-12</div>
-          <div>통신판매업 신고 번호: 제 2024-경남김해-1234호</div>
-          <div>대표: 김철수철수</div>
+          <div>{APP.COPYRIGHT}</div>
+          <div>{APP.SITE_NAME}</div>
+          <div>{APP.ADDRESS}</div>
+          <div>{APP.BUSINESS_REGISTRATION_NUMBER}</div>
+          <div>{APP.TELECOMMUNICATION_SALES_NUMBER}</div>
+          <div>{APP.REPRESENTATIVE}</div>
         </FooterContent>
 
         <FooterContent>
           <div>
-            <a
-              target="_blank"
-              href="https://github.com/marshot9420"
-              rel="noreferrer noopener"
-            >
+            <a target="_blank" href={APP.GITHUB_URL} rel="noreferrer noopener">
               <img src="/images/github.png" alt="MARSHOT Github" />
             </a>
           </div>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
+export * from './app';
 export * from './categories';
 export * from './urls';
